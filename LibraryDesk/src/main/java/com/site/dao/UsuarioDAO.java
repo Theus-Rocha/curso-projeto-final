5f24c2ed4c193c5ddc817a59b1a83ee5e38ad649
Matheus Rocha Silva <matheusilva2244@gmail.com>
2025-02-06 08:35:30 -0300

    Update UsuarioDAO.java
    Adições ao UsuarioDAO

--- LibraryDesk/src/main/java/com/site/dao/UsuarioDAO.java
+++ LibraryDesk/src/main/java/com/site/dao/UsuarioDAO.java
@@ -39,6 +39,64 @@ public class UsuarioDAO {
 			return false;
 		}
 	}
+
+	public boolean inserirU(Usuario usuario) {
+		String sql = "INSERT INTO Usuarios(Nome, idade, email, login, senha) VALUES(?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement stm = connection.prepareStatement(sql);
+			stm.setString(1, usuario.getNome());
+			stm.setString(2, usuario.getEmail());
+			stm.setString(3, usuario.getLogin());
+			stm.setString(4, usuario.getSenha());
+			stm.execute();
+			return true;
+		} catch(SQLException e)	{			
+			return false;
+		}
+	}
+	
+	public boolean deletarU(Usuario usuario) {
+		String sql = "DELETE FROM Usuarios WHERE id=?";
+		try {
+			PreparedStatement stm = connection.prepareStatement(sql);
+			stm.setInt(1, id);
+			stm.execute();
+			return true;
+		} catch(SQLException e) {
+			return false;
+		}
+	}
+	
+	public boolean alterarU(Usuario usuario) {
+		String sql = "UPDATE Usuarios SET Nome=?, idade=?, email=?, login=?, senha=?";
+		try {
+			PreparedStatement stm = connection.prepareStatement(sql);
+			stm.setString(1, usuario.getNome());
+			stm.setInt(2, usuario.getIdade);
+			stm.setString(3, usuario.getEmail);
+			stm.setString(4, usuario.getLogin);
+			stm.setString(5, usuario.getSenha);
+			stm.execute();
+			return true;
+		}catch(SQLException e) {
+			return false;
+		}
+	}
+	
+	
+	public boolean removerU(Usuario usuario) {
+		String sql = "DELETE FROM Usuarios WHERE id=?";
+		try {
+			PreparedStatemnt stm = connection.prepareStatement(sql);
+			stm.setInt(1, id);
+			stm.execute();
+			return true;
+		}catch(SQLException e) {
+			return false;
+		}
+	}
+
+	}
 }
 
 
